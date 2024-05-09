a13aad6881fd1f9ecb14d56ec2b1d24a0243ba2d
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-05-09 15:46:30 +0600

    added view more and view less button

--- src/Pages/Home/Chef/Chef.jsx
+++ src/Pages/Home/Chef/Chef.jsx
@@ -5,6 +5,7 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Chef = () => {
   const [chef, setChef] = useState([]);
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
     fetch("menu.json")
@@ -13,6 +14,9 @@ const Chef = () => {
         setChef(data);
       });
   }, []);
+
+  const visibleChefs = showAll ? chef : chef.slice(0, 6);
+
   return (
     <section className="mt-10 px-[5px]">
 
@@ -21,10 +25,18 @@ const Chef = () => {
         heading="Chef recommends"
         ></SectionTitle>
       <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-5">
-        {chef.map((s) => (
+        {visibleChefs.map((s) => (
           <SingleChef key={s._id} s={s}></SingleChef>
         ))}
       </div>
+
+      {chef.length > 6 ? (
+        <div className="flex justify-center items-center h-full mt-3">
+            <button className="cart-btn-view fontPrimary font-bold  uppercase text-orange-400" onClick={() => setShowAll(!showAll)}>
+          {showAll ? "View Less Chefs" : "View Full Chefs"}
+        </button>
+        </div>
+      ) : null}
     </section>
   );
 };
--- src/Pages/Home/Chef/SingleChef/SingleChef.jsx
+++ src/Pages/Home/Chef/SingleChef/SingleChef.jsx
@@ -1,21 +1,23 @@
+import "./SingleChef.css"
 
-
-const SingleChef = ({s}) => {
-
-    const {image,name,recipe}=s;
-    return (
-        
-<div className="card w-full bg-base-100 shadow-xl">
-  <figure><img className="w-full" src= {image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>{recipe}</p>
-    <div className="card-actions justify-center">
-      <button className="btn uppercase text-orange-400">Add to Cart</button>
+const SingleChef = ({ s }) => {
+  const { image, name, recipe } = s;
+  return (
+    <div className="card w-full bg-base-100 shadow-xl">
+      <figure>
+        <img className="w-full" src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-center">
+          <button className="cart-btn fontPrimary font-bold  uppercase text-orange-400">
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default SingleChef;

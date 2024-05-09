5830db79b58a2a77cdc3bb899d2ae92cb744e037
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-05-09 08:43:12 +0600

    navbar added

---
+++ src/Pages/Shared/Footer/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import Container from "../../../../components/Container/Container";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Shop</a>
+      </li>
+    </>
+  );
+  return (
+    <div id="navbar-part">
+      <Container>
+        <div className="navbar ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52"
+              >
+                {navOptions}
+              </ul>
+            </div>
+            <a className=" text-xl primaryFont">Bistro Boss</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;

37f4b4ddc60344108e15e958adab01acdbe9f29d
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-05-23 10:38:14 +0600

    added captcha

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,20 @@
 import Container from "../../components/Container/Container";
 import  "./Login.css"
 import loginImg from "../../assets/others/authentication2.png"
+import { Helmet } from "react-helmet-async";
+
+import { loadCaptchaEnginge, LoadCanvasTemplate,validateCaptcha } from 'react-simple-captcha'
+import { useEffect, useRef, useState } from "react";
+import Swal from 'sweetalert2'
+
+
 const Login = () => {
+    const [disabled,setDisabled]=useState(true)
+    const captchaRef=useRef()
+
+    useEffect(()=>{
+        loadCaptchaEnginge(6); 
+    },[])
 
     const handleToLogin= (event) =>{
         event.preventDefault()
@@ -9,12 +22,42 @@ const Login = () => {
         const email=form.email.value;
         const password=form.password.value;
         console.log(email,password)
+        // form.reset()
+
+    }
+
+    const handleValidateCaptcha =()=>{
+        const user_validate_captcha=captchaRef.current.value;
+        console.log(user_validate_captcha)
+        if(validateCaptcha(user_validate_captcha)){
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Captcha Matched",
+                showConfirmButton: false,
+                timer: 1500
+              });
+            setDisabled(false)
+            
+        }else{
+            setDisabled(true)
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Captcha didn't matched",
+                
+              });
+        }
 
     }
     
     return (
+
         
         <div id="login-part">
+            <Helmet>
+                <title>Bistro || Login</title>
+            </Helmet>
             <Container>
             <div className="hero ">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -44,7 +87,16 @@ const Login = () => {
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
-        <input className="btn bg-[#D1A054] fontPrimary uppercase text-white" type="submit" value="Login" />
+        <div className="form-control">
+          <label className="label">
+          <LoadCanvasTemplate />
+          </label>
+          <input type="text" placeholder="Write captcha" name="captcha" ref={captchaRef} className="input input-bordered" required />
+
+        </div>
+
+        <button onClick={handleValidateCaptcha} className="btn btn-outline ">Validation</button>
+        <input disabled={disabled}  className="btn bg-[#D1A054] fontPrimary uppercase text-white" type="submit" value="Login" />
       </form>
     </div>
   </div>

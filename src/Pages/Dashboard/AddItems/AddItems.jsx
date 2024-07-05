a32499f1b8677086a648a5c96bbbcce6963e3892
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-07-05 11:41:03 +0600

    added product by admin

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,12 +1,60 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Helmet } from "react-helmet-async";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure"
+import Swal from "sweetalert2";
+
+const image_hosting_key= import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api=`https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+console.log(image_hosting_key)
 
 
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+
+  const axiosPublic=useAxiosPublic();
+  const axiosSecure=useAxiosSecure()
+
+  const onSubmit = async(data) => {
+    const imageFile={ image: data.image[0] }
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers:{
+            "Content-Type":"multipart/form-data"
+        }
+        
+    })
+    if(res.data.success){
+      const menuItem={
+        name:data.name,
+        category:data.category,
+        price:parseFloat(data.price),
+        recipe:data.recipe,
+        image:res.data.data.display_url
+      }
+
+      const menuRes= await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data)
+      if(menuRes.data.insertedId){
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is added successfully`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+
+    }
+    console.log(res.data)
+  };
   return (
     <div className="mt-10">
+
+        <Helmet
+        title="Bistro || Add Item"
+        ></Helmet>
       <SectionTitle
         subHeading="What's new"
         heading="add an item"
@@ -36,18 +84,18 @@ const AddItems = () => {
               <div className="label primaryFont lg:text-1xl sm:text-xl font-bold">
                 <span className="label-text">Category*</span>
               </div>
-              <select
+              <select defaultValue="default"
                 {...register("category", {required:true})}
                 className="select select-bordered"
               >
-                <option disabled selected>
+                <option disabled value="default">
                   Select a category
                 </option>
                 <option value="salad">Salad</option>
                 <option value="pizza">Pizza</option>
                 <option value="soup">Soup</option>
                 <option value="dessert">Dessert</option>
-                <option value="drink">Drink</option>
+                <option value="drink">Drinks</option>
               </select>
             </label>
 
